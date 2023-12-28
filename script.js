e062547b4ccaa25e105fb49ce049be4cda2ed1f9
Tamerbekir <astoryofame@gmail.com>
2023-12-28 12:42:44 -0500

    reworked the script by removing and adding new LET statements/variables. Added IF statements under one FUNCTION to generate password. Added ELSE statements that would not include a specific character set if user did not select it along with alerts for each IF statement. User will get alert if no VARIABLES are selected. However, the process does not loop and user has to restart. Will fix. Added FOR LOOP and RETURN for PASSWORD but as of now not working properly

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+const password = generatePassword();
+const passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+passwordText.value = password;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -28,52 +28,80 @@ generateBtn.addEventListener("click", writePassword);
 
 //! Starting HW
 
-//arrays for the user to pick from
-let numbers = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-let symbols = ("~", "!", "@", "#", "$", "$", "%", "&", "*", "-", "_", "+","=");
-let lowercase = ("a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-let uppercase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-
 //length of password, user enter, which is 8-128
-let length = " ";
+let confirmLength = " ";
+
+//arrays for the user to pick from
+const numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+const symbols = ("~", "!", "@", "#", "$", "$", "%", "&", "*", "-", "_", "+","=");
+const lowercase = ("a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+const uppercase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+
+let confirmNumbers
+let confirmSymbols
+let confirmLowercase
+let confirmUppercase
+
+function generatePassword() {
+  alert("Please create a password");
+  confirmLength = prompt("Please enter a password between 8-128 characters long");
+  while (confirmLength <= 7 || confirmLength >= 129) {
+  //function will loop until user chooses a password between 8-128
+  confirmLength = prompt (`${confirmLength} does not meet requirements. Please choose between 8-128 characters`)
+}
+alert (`Your password will have ${confirmLength} characters`);
 
 let passwordCharacters = []
 
-let randomPassword = " "
+confirmNumbers = confirm("Do you want numbers in your password?");
 
-
-function generatePassword() {
-    alert("Please create a password");
-    length = prompt("Please enter a password between 8-128 characters long");
-    while (length <= 7 || length >= 129) 
-    //function will loop until user chooses a password between 8-128
-    length = prompt (`${length} does not meet requirements. Please choose between 8-128 characters`)
-    alert (`Your password will have ${length} characters`); 
-
-if (numbers) {
-confirm("Click OK to confirm if you would like to include numbers");
-  passwordCharacters = passwordCharacters.random(numbers)}
-
-if (symbols) {
-confirm("Click OK to confirm if you would like to include symbols"); 
-  passwordCharacters = passwordCharacters.random(symbols)}
+  if (confirmNumbers) {
+    passwordCharacters = passwordCharacters.concat(numbers)
+  } else {
+    alert("Numbers will not be used")
+    }
   
-if (lowercase) {
-confirm("Click OK to confirm if you would like to include lowercase characters"); 
-  passwordCharacters = passwordCharacters.random(lowercase)}
+  confirmSymbols = confirm("Do you want symbols in your password?");
 
-if (uppercase) {
-confirm("Click OK to confirm if you would like to include uppercase characters"); 
-    passwordCharacters = passwordCharacters.random(uppercase)}
+  if (confirmSymbols) {
+    passwordCharacters = passwordCharacters.concat(symbols)
+  } else {
+    alert("Symbols will not be used")
+    }
+  
+  confirmLowercase = confirm("Do you want lowercase letters in your password?");
+
+  if (confirmLowercase) {
+    passwordCharacters = passwordCharacters.concat(lowercase)
+  } else {
+    alert("Lowercase letters will not be used")
+    }
+  
+  confirmUppercase = confirm("Do you want uppercase letters in your password?");
+
+  if (confirmUppercase) {
+    passwordCharacters = passwordCharacters.concat(uppercase)
+  } else {
+    alert("Uppercase letters will not be used")
+    }
+
+  if (confirmNumbers === false && confirmSymbols === false && confirmLowercase === false && confirmUppercase === false) {
+    alert("You must select at least one character set.")
+  } while(confirmNumbers){
+  }
 
 
-console.log(passwordCharacters)
 
-for (let i = 0; i < length; i++){
-randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
 
-console.log(randomPassword) }
+  console.log(passwordCharacters)
+  
 
-alert("Here is your new passsowrd! Refresh the page if you would like a new generated password. Otherwise, do not forget to copy!")
-return randomPassword;
+    let randomPassword = " ";
+    
+    for (let i = 0; i < confirmLength; i++) {
+      randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      console.log(randomPassword)
+    }
+    return randomPassword;
 }
+
