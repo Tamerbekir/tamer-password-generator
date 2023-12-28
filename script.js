6b54a61def024c89962f59f40b0cf6f017607f43
Tamerbekir <astoryofame@gmail.com>
2023-12-27 23:58:49 -0500

    added let for password characters and randompassword. Added if statements for all characters. I am able to run through prompt BUT getting a error message in console. Need to change RANDOM syntax used for let statements.

--- script.js
+++ script.js
@@ -29,20 +29,51 @@ generateBtn.addEventListener("click", writePassword);
 //! Starting HW
 
 //arrays for the user to pick from
-const number = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-const symbols = ("~", "!", "@", "#", "$", "$", "%", "&", "*", "-", "_", "+","=");
-const lowercase = ("a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-const uppercase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+let numbers = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+let symbols = ("~", "!", "@", "#", "$", "$", "%", "&", "*", "-", "_", "+","=");
+let lowercase = ("a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+let uppercase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
 
 //length of password, user enter, which is 8-128
 let length = " ";
+
+let passwordCharacters = []
+
+let randomPassword = " "
+
 
 function generatePassword() {
     alert("Please create a password");
     length = prompt("Please enter a password between 8-128 characters long");
     while (length <= 7 || length >= 129) 
     //function will loop until user chooses a password between 8-128
-    length = prompt ("Password does not meet requirements. Please choose between 8-128 characters")
+    length = prompt (`${length} does not meet requirements. Please choose between 8-128 characters`)
     alert (`Your password will have ${length} characters`); 
-    }
+
+if (numbers) {
+confirm("Click OK to confirm if you would like to include numbers");
+  passwordCharacters = passwordCharacters.random(numbers)}
+
+if (symbols) {
+confirm("Click OK to confirm if you would like to include symbols"); 
+  passwordCharacters = passwordCharacters.random(symbols)}
   
+if (lowercase) {
+confirm("Click OK to confirm if you would like to include lowercase characters"); 
+  passwordCharacters = passwordCharacters.random(lowercase)}
+
+if (uppercase) {
+confirm("Click OK to confirm if you would like to include uppercase characters"); 
+    passwordCharacters = passwordCharacters.random(uppercase)}
+
+
+console.log(passwordCharacters)
+
+for (let i = 0; i < length; i++){
+randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+
+console.log(randomPassword) }
+
+alert("Here is your new passsowrd! Refresh the page if you would like a new generated password. Otherwise, do not forget to copy!")
+return randomPassword;
+}
