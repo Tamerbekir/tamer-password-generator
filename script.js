e9809cd10bf115f171f3b796c1df4a810453a06f
Tamerbekir <astoryofame@gmail.com>
2023-12-27 19:38:08 -0500

    changed wording from 'Sepcial characters to Symbols' and changed arrays to be singlular characters and not one

--- script.js
+++ script.js
@@ -30,10 +30,10 @@ generateBtn.addEventListener("click", writePassword);
 
 // using let for declaration
 let confirmLength = " ";
-let confirmSpecialCharacter;
-let confirmNumericCharacter;
-let confirmUpperCase;
+let confirmNumbers;
+let confirmSymbols;
 let confirmLowerCase;
+let confirmUppercase;
 
 // using const for arrays
 const numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
@@ -44,12 +44,12 @@ const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 
 // Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
-  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+  const confirmLength = (prompt("How many characters would you like your password to contain?"));
 
   // Loop if answer is outside the parameters 
   while(confirmLength <= 7 || confirmLength >= 51) {
       alert("Password length must be between 8-50 characters Try again");
-      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+      const confirmLength = (prompt("How many characters would you like your password to contain?"));
       } 
 
       // Repeat back how many charactes the user will have  
