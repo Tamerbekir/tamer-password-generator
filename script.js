3248f9c7599de2f9bd8333efe82b6c1736f06bc6
Tamerbekir <astoryofame@gmail.com>
2023-12-28 19:10:11 -0500

    PASSWORD GENERATOR WORKING! Woohoo! Fixed issue where generator would only output/randomize the last symbol, letter or number by making numbers, letters and symbol one long string instead of individual strings using quotes. Added notes to lines. Added the feature that informs user if they chose to NOT select a particular character in their password

--- script.js
+++ script.js
@@ -28,15 +28,23 @@ generateBtn.addEventListener("click", writePassword);
 
 //! Starting HW
 
-//length of password, user enter, which is 8-128
-let confirmLength = " ";
+//length of password, user enter, which is 8-128.
+//empty STRING that gets filled by the users slected password amount.
+let confirmLength = "";
 
-//arrays for the user to pick from
-const numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-const symbols = ("~", "!", "@", "#", "$", "$", "%", "&", "*", "-", "_", "+","=");
-const lowercase = ("a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-const uppercase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+//ARRAYS that are going to be used to generate the password
+// let numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+// let symbols = ("~", "!", "@", "#", "$", "$", "%", "&", "*", "-", "_", "+","=");
+// let lowercase = ("a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+// let uppercase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
 
+let numbers = (123456789);
+let symbols = ("~!@#$%&*-_+=<>");
+let lowercase = ("abcdefghijklmnopqrstuvwxyz");
+let uppercase = ("ABCDEFGHIJKLMNOPQRSTUVQXYZ");
+
+
+// VARIABLES the user picks from 
 let confirmNumbers
 let confirmSymbols
 let confirmLowercase
@@ -51,7 +59,8 @@ function generatePassword() {
 }
 alert (`Your password will have ${confirmLength} characters`);
 
-let passwordCharacters = []
+//empty VARIABLE STRING that will get filled once user confrims selection(s)
+let passwordCharacters = "";
 
 confirmNumbers = confirm("Do you want numbers in your password?");
 
@@ -84,23 +93,21 @@ confirmNumbers = confirm("Do you want numbers in your password?");
   } else {
     alert("Uppercase letters will not be used")
     }
-
+    //if user selects none of the VARIABLES, user will get an alert.
   if (confirmNumbers === false && confirmSymbols === false && confirmLowercase === false && confirmUppercase === false) {
+    //No loop in place to take user back to the original options if no paramaters are selected. Will fix!
     alert("You must select at least one character set.")
-  } while(confirmNumbers){
   }
 
+  //empty STRING that is filled by the random password generated, which will be used in the FOR LOOP. 
+  let randomPassword = "";
 
-
-
-  console.log(passwordCharacters)
   
+  for (let i = 0; i < confirmLength; i++) {
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword)
+    console.log(passwordCharacters)
 
-    let randomPassword = " ";
-    
-    for (let i = 0; i < confirmLength; i++) {
-      randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-      console.log(randomPassword)
     }
     return randomPassword;
 }
